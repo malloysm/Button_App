69b5a781b3359a48c7ffa96621e47d818bda252a
malloysm <malloysm@miamioh.edu>
2019-02-02 18:30:58 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace _387_Button
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ButtonAction.Text == "") ButtonAction.Text = "You've pressed the Button";
+            else if (ButtonAction.Text == "You've pressed the Button") ButtonAction.Text = "You did it again";
+            else if (ButtonAction.Text == "You did it again") ButtonAction.Text = "Ok, you can stop now";
+            else if (ButtonAction.Text == "Ok, you can stop now") ButtonAction.Text = "Stop";
+            else if (ButtonAction.Text == "Stop") ButtonAction.Text = "...";
+            else ButtonAction.Text = "Stop";
+        }
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    ToggleAction.Text = ":)";
+                }
+                else
+                {
+                    ToggleAction.Text = "";
+                }
+            }
+        }
+    }
+}
